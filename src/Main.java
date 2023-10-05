080bdf4800f390d1df9a8d0c7bd13ece85620b63
mpocampod <mpocampod@eafit.edu.co>
2023-10-05 17:58:27 -0500

    cambios

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Capitan capVelero = new Capitan("Juan", "Perez", "DRZ628IAU");
+        Capitan capVelero = new Capitan("Juan", "Perez", "DRZ628IA");
         Velero vel = new Velero(capVelero, 1000.0, 20000.0, 2022, 15.0, 5);
 
-        Capitan capYate = new Capitan("Maria", "Lopez", "67890");
+        Capitan capYate = new Capitan("Maria", "Lopez", "DPO231SNJ");
         Yate yate = new Yate(capYate, 2000.0, 20000.0, 2019, 20.0, 8);
 
         vel.validar();
